415202b3a8416fce5f818bd50be334f0972f9a9c
Shamshodbek Toshqobilov <shamshodbektoshqobilov@gmail.com>
2026-07-01 14:27:54 +0500

    fix(backend): allow docker proxy host for auth

--- backend/config/settings.py
+++ backend/config/settings.py
@@ -11,6 +11,7 @@ environ.Env.read_env(BASE_DIR.parent / ".env")
 SECRET_KEY = env("SECRET_KEY")
 DEBUG = env("DEBUG")
 ALLOWED_HOSTS = env.list("ALLOWED_HOSTS", default=["localhost", "127.0.0.1"])
+ALLOWED_HOSTS += [host for host in ["backend", "web"] if host not in ALLOWED_HOSTS]
 
 DJANGO_APPS = [
     "django.contrib.admin",
